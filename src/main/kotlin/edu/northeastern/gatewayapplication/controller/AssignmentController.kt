bc1e4e49264252b83f6aa4d87caa8fed5e8f4740
Goutham K <kanahasabai.g@northeastern.edu>
2023-10-18 19:36:00 -0400

    Revert "fix: assignment-5; added deadline date validation"
    This reverts commit 67521450db901ff66c78221639d854668cdc26d1.

--- src/main/kotlin/edu/northeastern/gatewayapplication/controller/AssignmentController.kt
+++ src/main/kotlin/edu/northeastern/gatewayapplication/controller/AssignmentController.kt
@@ -5,11 +5,20 @@ import edu.northeastern.gatewayapplication.service.AccountService
 import edu.northeastern.gatewayapplication.service.AssignmentService
 import edu.northeastern.gatewayapplication.utils.Utils
 import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.HttpRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.Authentication
-import org.springframework.web.bind.annotation.*
-import java.util.*
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 private val logger = mu.KotlinLogging.logger {}
 
@@ -44,18 +53,7 @@ class AssignmentController(
     fun updateAssignment(authentication: Authentication, @PathVariable id: UUID, @RequestBody assignment: Assignment): ResponseEntity<Assignment> {
         logger.info { "Updating assignment for user ${authentication.name}" }
 
-        if (utils.areMandatoryFieldsPresent(assignment).not() ||
-            utils.isValidPoints(assignment.points!!).not() ||
-            utils.isValidAttempts(assignment.attempts!!).not() ||
-            utils.isValidDate(assignment.deadline!!).not()
-        ) {
-            return ResponseEntity.badRequest().build()
-        }
-
         val assignmentInDB = assignmentService.get(id)
-        if (assignmentInDB.isPresent.not()) {
-            return ResponseEntity.notFound().build()
-        }
 
         val requestUser = accountService.getByEmail(authentication.name)
         val userOfAssignment = assignmentInDB.get().user
@@ -69,6 +67,9 @@ class AssignmentController(
             return ResponseEntity.notFound().build()
         }
 
+        if (utils.areMandatoryFieldsPresent(assignment).not() || utils.isValidPoints(assignment.points!!).not()) {
+            return ResponseEntity.badRequest().build()
+        }
 
         // 400 if points are not in range
         if (utils.isValidPoints(assignment.points!!).not()) {
@@ -93,11 +94,7 @@ class AssignmentController(
     fun createAssignment(authentication: Authentication, @RequestBody assignment: Assignment): ResponseEntity<Assignment> {
         logger.info { "Creating assignment for user ${authentication.name}" }
         // 400 if points are not in range or mandatory fields are not present
-        if (utils.areMandatoryFieldsPresent(assignment).not() ||
-            utils.isValidPoints(assignment.points!!).not() ||
-            utils.isValidAttempts(assignment.attempts!!).not() ||
-            utils.isValidDate(assignment.deadline!!).not()
-            ) {
+        if (utils.areMandatoryFieldsPresent(assignment).not() || utils.isValidPoints(assignment.points!!).not()) {
             return ResponseEntity.badRequest().build()
         }
 
